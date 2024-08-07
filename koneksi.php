099f428ee4c70fa962e0493872f8c33b63f6217e
adikusmawan <59168931+adikusmawan@users.noreply.github.com>
2024-08-07 22:05:57 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
  
-$konek = mysqli_connect("localhost","root","","bansosptpos");
+$konek = mysqli_connect("localhost","root","","user=postgres.lmddghsviijdfgeiiqsd password=host=aws-0-ap-southeast-1.pooler.supabase.com port=6543 dbname=postgres");
  
 // Check connection
 if (mysqli_connect_errno()){
